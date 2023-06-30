d43f81fde92be28fa44e1f6fe2b231d6780abcdb
rahimaaa <rakhimakhabib@gmail.com>
2023-06-30 14:07:08 -0400

    added seed and cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
+const cors = require('cors');
 const db = require("./db");
 const PORT = "8080";
 
 const app = express();
-
+app.use(cors());
 // Mount on API
 app.use("/api", require("./api"));
 
 // Syncing DB Function
-const syncDB = () => db.sync();
+const syncDB =() =>
+  db.sync();
 
 // Run server function
 const serverRun = () => {
@@ -16,6 +18,7 @@ const serverRun = () => {
     console.log(`Live on port: ${PORT}`);
   });
 };
+
 
 syncDB();
 serverRun();
--- seed.js
+++ seed.js
@@ -1,4 +1,4 @@
-const db = require("./db");
+const db = require("./db/db");
 const { Students, Campus } = require("./db/models");
 
 const seedStudents = [
@@ -7,7 +7,9 @@ const seedStudents = [
   { first_name: "Xu", last_name: "Wang", email: "xuWang@mail.com", 
   imageUrl: "sdff ", gpa: 4.0},
   { first_name: "Allan James", last_name: "Lapid", email: "aj@gmail.com", 
-  imageUrl: "fsf", gpa: 2.0},
+  imageUrl: "fsf", gpa: 3.0},
+  {first_name: "Depak ", last_name: "A", email: "depak@gmail.com", 
+  imageUrl: "fssdf", gpa: 3.0}
 ];
 
 const seedCampus = [
@@ -15,9 +17,44 @@ const seedCampus = [
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitijhghgghfjdjhgdjfhgjsbhbhg hghbhbfvhjsbvjhbdfhbvhbvdfvbfv  vhbfhbc  hdbvhbjd vj djhvbjbvjhdbfjvbdfvjhbfjhdbjdhfb dfh hf j"},
   
 ];
+
+
 const seed = async () => {
-  await Students.bulkCreate(seedStudents);
-  await Campus.bulkCreate(seedCampus);
+  // Check if the data already exists before inserting
+  const existingStudents = await Students.findAll();
+  const existingCampus = await Campus.findAll();
+
+  if (existingStudents.length === 0 && existingCampus.length === 0) {
+    await Students.bulkCreate(seedStudents);
+    await Campus.bulkCreate(seedCampus);
+    console.log("Seeding complete");
+  } else {
+    console.log("Data already exists, skipping seeding");
+  }
 };
 
 seed().then(() => process.exit());
+
+
+
+
+// const seed = async () => {
+//   // Delete existing data
+//   await Students.destroy({ truncate: true });
+//   await Campus.destroy({ truncate: true });
+
+//   // Insert new data
+//   await Students.bulkCreate(seedStudents);
+//   await Campus.bulkCreate(seedCampus);
+//   console.log("Seeding complete");
+// };
+
+// seed().then(() => process.exit());
+
+
+// const seed = async () => {
+//   await Students.bulkCreate(seedStudents);
+//   await Campus.bulkCreate(seedCampus);
+// };
+
+// seed().then();
