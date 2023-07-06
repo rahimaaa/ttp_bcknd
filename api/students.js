f41ed6028000610feaa0a5407d34f721331372ca
Unknown-Pplayer <xwang2230@gmail.com>
2023-07-06 13:32:50 -0400

    adding student

--- api/students.js
+++ api/students.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { Students } = require("../db/models");
+const { Student } = require("../db/models");
 
-// Root here is localhost:8080/api/shoes/
 router.get("/", async (req, res, next) => {
   try {
-    // Shoes.findAll() ===  SELECT * FROM students
-    const allStudents = await Students.findAll();
+    const allStudents = await Student.findAll();
 
     allStudents
-      ? res.status(200).json(allStudents) 
-      : res.status(404).send("Shoes List Not Found"); 
+      ? res.status(200).json(allStudents)
+      : res.status(404).send("Students List Not Found");
   } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/AddStudent", async (req, res, next) => {
+  try {
+    const createStudent = await Student.create(req.body);
+    res.status(201).json(createStudent);
+  } catch (error) {
+    console.error(error);
     next(error);
   }
 });
--- db/models/index.js
+++ db/models/index.js
@@ -1,14 +1,14 @@
-const Students= require("./students");
+const Student = require("./student");
 const Campus = require("./campus");
 
 // Associations Go Here
 
 // Many to Many
 // We set a through table "ShopperShoes" that handles the M-M relationship
-Campus.hasMany(Students);
-Students.belongsTo(Campus);
+Campus.hasMany(Student);
+Student.belongsTo(Campus);
 
 module.exports = {
-  Students,
+  Student,
   Campus,
 };
